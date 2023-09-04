f19ef3d7e18b49c1f28b42fc12fe38136d369b92
Athulya V Ajayakumar <87646779+athulya-ajayakumar@users.noreply.github.com>
2023-09-04 18:51:37 +0530

    Update chess.ts

--- src/chess.ts
+++ src/chess.ts
@@ -1,28 +1,6 @@
 /**
- * @license
- * Copyright (c) 2023, Jeff Hlywa (jhlywa@gmail.com)
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ done by athulya
+
  */
 
 export const WHITE = 'w'
@@ -126,9 +104,9 @@ const BITS: Record<string, number> = {
 }
 
 /*
- * NOTES ABOUT 0x88 MOVE GENERATION ALGORITHM
+ * \
  * ----------------------------------------------------------------------------
- * From https://github.com/jhlywa/chess.js/issues/230
+ * 
  *
  * A lot of people are confused when they first see the internal representation
  * of chess.js. It uses the 0x88 Move Generation Algorithm which internally
